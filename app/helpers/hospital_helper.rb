db723175b0a711709fb2af8092d44a224fcd9004
JackHou <vjack070707@gmail.com>
2015-02-18 00:49:49 +0800

    edit

--- app/helpers/hospital_helper.rb
+++ app/helpers/hospital_helper.rb
@@ -1,15 +1,19 @@
 module HospitalHelper
     def HospitalHelper.distance_disaster_to_hospital(geo_hash)
         d_to_hospital = []
+        haved = []
         hospital = Hospitals.all
         hospital.each do |data|
             geo_hash.each do |id, content|
                 each_geo = [content[:lat], content[:lng]]
                 dis = Geocoder::Calculations.distance_between(each_geo, [data["lat"],data["lng"]]).round(3)
                 if dis <= 4 # if the hospital is in 4 km distance, return 
-                    d_to_hospital << {name: data["name"], distance: dis, lat: data["lat"], lng: data["lng"], address: data["address"],
-                            report_full: data["report_full"], wait_see: data["wait_see"], wait_push_bed:data["wait_push_bed"],
-                            wait_bed: data["wait_bed"], wait_cure_bed: data["wait_cure_bed"]}
+                    unless haved.include? data["name"]
+                        haved << data["name"]
+                        d_to_hospital << {name: data["name"], distance: dis, lat: data["lat"], lng: data["lng"], address: data["address"],
+                                report_full: data["report_full"], wait_see: data["wait_see"], wait_push_bed:data["wait_push_bed"],
+                                wait_bed: data["wait_bed"], wait_cure_bed: data["wait_cure_bed"]}
+                    end
                 end
             end
         end
