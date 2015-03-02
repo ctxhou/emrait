ef41070e83c3bff142ffcc7809af525c72c588de
JackHou <vjack070707@gmail.com>
2015-03-02 15:25:03 +0800

    add icon; loading suggestion

--- app/assets/javascripts/clinic_search.js
+++ app/assets/javascripts/clinic_search.js
@@ -10,6 +10,7 @@ var Clinics = require('./collections/clinic_location')
 // view
 var ClinicListView = require('./views/clinic/clinic_list')
 var ClinicMapView = require('./views/clinic/clinic_map')
+var Spinner = require('spin')
 
 var GetGeo = require('./module/get_geo')
 require("./vendor/jquery.tinyMap-3.1.3.min")
@@ -49,6 +50,24 @@ clinic_app.on("before:start", function() {
         "皮膚科":"/assets/map/skin.png",
         "家醫科": "/assets/map/home.png"
     }
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 20, // The length of each line
+      width: 10, // The line thickness
+      radius: 30, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9 // The z-index (defaults to 2000000000)
+    };
+    var target = document.getElementById('spin');
+    var spinner = new Spinner(opts).spin(target);
     var that = this;
     var geo = [$("#lat_val").val(), $("#lng_val").val()]
     var map = $("#gmap").tinyMap({
--- app/assets/javascripts/shelter_result.js
+++ app/assets/javascripts/shelter_result.js
@@ -10,6 +10,7 @@ var ShelterResultView = require('./views/shelter/shelter_result');
 var Ambulance = require('./models/ambulance');
 var Shelters = require('./collections/shelter');
 var bootstrap = require('bootstrap')
+var Spinner = require('spin')
 var app = new Marionette.Application();
 app.addRegions({
     content: "#content"
@@ -19,6 +20,24 @@ app.on("before:start", function() {
     var disaster_lat = $("#lat").val();
     var disaster_lng = $("#lng").val();
     var shelters = new Shelters();
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 20, // The length of each line
+      width: 10, // The line thickness
+      radius: 30, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9 // The z-index (defaults to 2000000000)
+    };
+    var target = document.getElementById('spin');
+    var spinner = new Spinner(opts).spin(target);
     shelters.fetch({
         success: function() {
             var shelterResultView = new ShelterResultView({collection: shelters});
