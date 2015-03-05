b61f846288b326d9128eb5e3ae26bf12d2d902ba
JackHou <vjack070707@gmail.com>
2015-03-05 18:31:06 +0800

    add uglify file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var requireDir = require('require-dir');
 var gulp = require('gulp');
+var config = require('./gulpfile/config');
+var uglify = require('gulp-uglify');
+var dist = './app/assets/javascripts/dist/';
 
 requireDir('./gulpfile', { recurse: true });
 
 
 gulp.task('default', ['browserify', 'browser-sync']);
+
+
+
+gulp.task('compress', function() {
+    gulp.src("./app/assets/javascripts/dist/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest(dist))
+});
