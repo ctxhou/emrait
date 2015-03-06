955794de7dd553ca68826e8523ac4c410e6947c9
JackHou <vjack070707@gmail.com>
2015-03-06 23:48:07 +0800

    add assign instruction

--- app/assets/javascripts/ai_ambulance.js
+++ app/assets/javascripts/ai_ambulance.js
@@ -10,6 +10,7 @@ var bootstrap = require('bootstrap');
 var AmbulanceView = require('./views/ambulance/ambulance_modal');
 var Ambulance = require('./models/ambulance');
 var Hospital = require('./models/hospital');
+var Spinner = require('spin')
 require("./vendor/jquery.tinyMap.min")
 require("./vendor/readmore.min")
 
@@ -75,6 +76,25 @@ app.on("before:start", function() {
     });
 
     $("#assign-submit").click(function(event) {
+        var opts = {
+          lines: 13, // The number of lines to draw
+          length: 20, // The length of each line
+          width: 10, // The line thickness
+          radius: 30, // The radius of the inner circle
+          corners: 1, // Corner roundness (0..1)
+          rotate: 0, // The rotation offset
+          direction: 1, // 1: clockwise, -1: counterclockwise
+          color: '#000', // #rgb or #rrggbb or array of colors
+          speed: 1, // Rounds per second
+          trail: 60, // Afterglow percentage
+          shadow: false, // Whether to render a shadow
+          hwaccel: false, // Whether to use hardware acceleration
+          className: 'spinner', // The CSS class to assign to the spinner
+          zIndex: 2e9 // The z-index (defaults to 2000000000)
+        };
+        $(".spin-container").show();
+        var target = document.getElementById('spin');
+        var spinner = new Spinner(opts).spin(target);
         var json = {}
         json["assign"] = []
         $(".js-assign").each(function(i, obj) {
