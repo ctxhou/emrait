9eac5dd9de03d4a5699ced5694a47daa053f4e3c
JackHou <vjack070707@gmail.com>
2015-03-15 21:48:40 +0800

    migrate to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150217143631) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ambulances", force: true do |t|
     t.integer  "seq_id"
     t.string   "site_type"
